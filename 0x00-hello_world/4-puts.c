a851b68dd3eb341ec2dfb60ef68a61e1f8237d09
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2023-03-16 17:05:18 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
-*main - entry point
+* main - Entry point
 *
-*Return: Always 0 (Success)
+* Return: Always 0 (Success)
 */
- int main(void)
- {
-    puts ("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
- }
+}
